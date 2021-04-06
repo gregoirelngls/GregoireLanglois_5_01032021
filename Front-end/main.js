2673da667c358f5bd699921a8f18f474f43a8e1d
Gregoire Langlois <gregoire.p.langlois@gmail.com>
2021-04-06 10:46:43 +0200

    Finalisation page produit

--- Front-end/main.js
+++ Front-end/main.js
@@ -9,28 +9,63 @@ request.onreadystatechange = function(){
 
 
         //Récupère toutes les cameras
-        var cameras = JSON.parse(request.responseText);
-        // Pour chaque camera
-        cameras.forEach(function(camera){
-            //On créer un div caemra (pour linstant juste console.log)
-            createDivCamera(camera);
-        });
+        let cameras = JSON.parse(request.responseText);
+        console.log(cameras);
+        renderHTML(cameras)
     }
 }
 
-function createDivCamera (camera) {
-    //debug
-    console.log("id" + camera._id);
-    console.log(camera);
-    //créer une div enfant
-    const newElt = document.createElement("div");
-    //récupère id camera parent
-    let elt = document.getElementById("Cameras");
-    //ajouter un id à ma div enfant
-    newElt.setAttribute("id", camera._id);
-    //mettre la div enfant dans la div parent
-    elt.appendChild(newElt);
-}
+// //function createDivCamera (camera) {
+//     //debug
+//     console.log("id" + camera._id);
+//     console.log(camera);
+//     //créer une div 
+//     const divParent = document.createElement("div");
+//     //récupère id camera parent
+//     let sectionCameras = document.getElementById("Cameras");
+//     //ajouter un id à ma div parent
+//     divParent.setAttribute("id", camera._id);
+//     //mettre la div parent dans la div sectionCameras
+//     sectionCameras.appendChild(divParent);
+//     //créer une figure
+//     const myFigure = document.createElement('figure');
+//     // je recupère le tag name "divParent"
+//     let figure = document.getElementById("divparent");
+//     // j'ajoute la figure à la div parent
+//     divParent.appendchild(myFigure);
+//     // créeation d'une figcaption
+//     const myFigcaption = document.createElement('Figcaption');
+//     // je recupère la figure
+//     let Figcaption = document.getElementById("figure");
+//     // j'ajoute la Figcaption à ma figure
+//     Figure.appendchild(myFigcaption);
+//     // je créer un lien "a"
+//     const myLink = document.createElement('a');
+//     //j'ajoute mon lien "a" à ma figcaption
+//     Link.appendchild(myLink);
+
+    function renderHTML(cameras) { //fonction de création du contenu HTML
+
+        const div = document.createElement('div'); //création de la div qui accueillera le contenu
+    
+        for (i = 0; i < cameras.length; i++) { //exécute la fonction pour chaque App photo.
+            div.innerHTML += `<div class="camera"> 
+            <a class="buttonAjouter" href="produit.html?id=${cameras[i]._id}">
+            <img src= ${cameras[i].imageUrl} alt = "app" class="camera-img" \>
+            <h2 class="card-title"> ${cameras[i].name}</h2>
+            <p class="card-text">${cameras[i].description}</p>
+            <p class="card-price">${cameras[i].price /100} €</p>
+            </a>
+            </div>`
+        }
+        const sectionCameras = document.getElementById("Cameras")
+        sectionCameras.appendChild(div); //le contenu (ci-dessus) est placé dans la div "Cameras"(html)
+    };
+    
+    
+
+
+
 
 
 
